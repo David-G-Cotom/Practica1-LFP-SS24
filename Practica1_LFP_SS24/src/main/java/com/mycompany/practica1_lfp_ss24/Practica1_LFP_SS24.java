ae5e853e7efa2084bceca64aa15234c51031bf47
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2024-08-21 12:38:55 -0600

    Agregando la carpeta de la Practica

---
+++ Practica1_LFP_SS24/src/main/java/com/mycompany/practica1_lfp_ss24/Practica1_LFP_SS24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1_lfp_ss24;
+
+/**
+ *
+ * @author Carlos Cotom
+ */
+public class Practica1_LFP_SS24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
